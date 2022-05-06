1915e1a893ba51031a7b0de8462831fc28134750
Ashutosh Pandey <94055977+abtp2@users.noreply.github.com>
2022-05-07 00:31:52 +0530

    Add files via upload

--- GetWeb.js
+++ GetWeb.js
@@ -3,3 +3,7 @@ var copyText = document.getElementById("alert");
 alert(copyText.value);
 }
 
+function target(){
+document.getElementById("target-bc-white-c-black").style.background ="#fff";
+document.getElementById("target-bc-white-c-black").style.color ="#000";
+}
